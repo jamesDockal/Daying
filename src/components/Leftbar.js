d6f8700f1e3523eb71f61c18ad50d7d7abc2fd38
james dockal <jamesistranho@gmail.com>
2021-06-21 18:39:50 -0300

    first commit

---
+++ src/components/Leftbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { LeftbarContaier, Today } from "../styles/leftbar";
+
+export default function Leftbar() {
+  return (
+    <LeftbarContaier>
+      <Today>
+        <h1>Today</h1>
+      </Today>
+    </LeftbarContaier>
+  );
+}
---
+++ src/styles/leftbar.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const LeftbarContaier = styled.div`
+  width: 33vw;
+  background-color: white;
+  height: 92vh;
+  margin: 1rem;
+  border-radius: 1rem;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const Today = styled.div`
+  display: flex;
+`;
